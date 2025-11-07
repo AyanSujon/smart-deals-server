3dc52a49e07556fee1108732b5d5759b53a442da
Ayan <sujon.seo.bd@gmail.com>
2025-11-07 12:02:05 +0600

    added to vercel

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/', (req, res)=> {
 
 async function run (){
     try{
-        await client.connect();
+        // await client.connect();
 
         const db = client.db('smart_db');
         const productsCollection = db.collection('products');
@@ -156,7 +156,7 @@ async function run (){
 
 
 
-        await client.db("admin").command({ping: 1});
+        // await client.db("admin").command({ping: 1});
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     }
